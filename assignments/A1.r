fa01b4662b39a7e98be174e16bfab20f20f640eb
Ivar Gaitan <ivar.gaitan@embark-studios.com>
2025-09-16 19:34:27 +0200

    Problem 1a

---
+++ assignments/A1.r
@@ -0,0 +1,41 @@
+# Problem 1
+# Let y1..yn | theta ~ Bern(theta), and assume that you have obtained a sample 
+# with s=14 successes in n=20 trials. Assume a Beta(alfa_0, beta_0) prior for 
+# theta and let alfa_0 = beta_0 = 2.
+
+n = 20
+s = 14
+f = n-s
+alfa_0 = beta_0 = 2
+
+# a)
+# Draw random numbers from the posterior theta | y ~ Beta(alfa_0 + s, beta_0 + f),
+# where y = (y_1, ... , y_n) and verify graphically that the Monte Carlo (MC) 
+# estimates of the posterior mean and standard deviation converges to the true 
+# values as the number of random draws grows large.
+
+# Posterior parameters
+alfa_p = alfa_0 + s
+beta_p = beta_0 + f
+
+# Posterior mean and variance
+postMean = alfa_p / (alfa_p + beta_p)
+postVar = (alfa_p * beta_p)/((alfa_p + beta_p)^2 * (alfa_p + beta_p + 1))
+
+numSamples = 1000
+post = rbeta(numSamples, alfa_p, beta_p)
+
+sampleMeans = seq(1, numSamples)
+sampleVars = seq(1, numSamples)
+for (i in 1:numSamples) {
+  sample = rbeta(i, alfa_p, beta_p)
+  sampleMeans[i] = sum(sample)/i
+  sampleVars[i] = var(sample)
+}
+
+
+plot(sampleMeans, type="l", ylim=c(0, 1))
+lines(sampleVars)
+
+
+
