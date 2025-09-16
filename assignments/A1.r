ad16505cd665983a3e4bdd3ef2d922f9a4624311
Ivar Gaitan <ivar.gaitan@embark-studios.com>
2025-09-16 22:55:43 +0200

    Update plots

--- assignments/A1.r
+++ assignments/A1.r
@@ -20,9 +20,9 @@ beta_p = beta_0 + f
 
 # Posterior mean and variance
 postMean = alfa_p / (alfa_p + beta_p)
-postVar = (alfa_p * beta_p)/((alfa_p + beta_p)^2 * (alfa_p + beta_p + 1))
+postVar = sqrt((alfa_p * beta_p)/((alfa_p + beta_p)^2 * (alfa_p + beta_p + 1)))
 
-numSamples = 1000
+numSamples = 10000
 post = rbeta(numSamples, alfa_p, beta_p)
 
 sampleMeans = seq(1, numSamples)
@@ -30,12 +30,20 @@ sampleVars = seq(1, numSamples)
 for (i in 1:numSamples) {
   sample = rbeta(i, alfa_p, beta_p)
   sampleMeans[i] = sum(sample)/i
-  sampleVars[i] = var(sample)
+  sampleVars[i] = sqrt(var(sample))
 }
 
+par(mfcol=c(1,2))
 
-plot(sampleMeans, type="l", ylim=c(0, 1))
-lines(sampleVars)
+plot(sampleMeans, type="l", yaxt="n", xlab="Sample size", ylab="", col="steelblue", main="Sample mean")
+abline(a=postMean, b=0, lty="dashed")
+axis(2, at=c(postMean), labels=c("Posterior mean"))
+#axis(2, at=c(min(sampleMeans), postMean, max(sampleMeans)),
+#     labels=c(round(min(sampleMeans), digits=2), "Posterior mean", round(max(sampleMeans), digits=2)))
 
+plot(sampleVars, type="l", xlab="Sample size", ylab="", col="indianred", yaxt="n", main="Sample sd")
+abline(a=postVar, b=0, lty="dashed")
+axis(2, at=c(postVar), labels=c("Posterior sd"))
 
+par(mfcol=c(1,1))
 
